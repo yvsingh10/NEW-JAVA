7b72d3f24929f1c28fb419823088985fff65b5c7
Yash Vardhan Singh <yvsingh102@gmail.com>
2026-09-24 00:03:08 +0530

    Add Arithmetic class for basic operations

---
+++ Experiments/6.EXP.java
@@ -0,0 +1,13 @@
+class Arithmetic
+{
+    public static void main(String[] args)
+    {
+        int a = 20;
+        int b = 5;
+
+        System.out.println("Addition = " + (a + b));
+        System.out.println("Subtraction = " + (a - b));
+        System.out.println("Multiplication = " + (a * b));
+        System.out.println("Division = " + (a / b));
+    }
+}
